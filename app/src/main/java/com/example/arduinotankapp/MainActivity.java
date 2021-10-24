c4ad77d29e99c75623efe0ce49ad110a101dc597
Jonathan <jonathanmistishin@gmail.com>
2021-10-24 14:05:36 -0400

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/arduinotankapp/MainActivity.java
+++ app/src/main/java/com/example/arduinotankapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.arduinotankapp;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
