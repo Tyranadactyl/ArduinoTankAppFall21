7fe45e52044dac2b831ecc76cdb2a1e75427bdd0
Jonathan <jonathanmistishin@gmail.com>
2021-10-29 17:02:12 -0400

    Fixed the entire app crashing on open, and added the code to send HTTP signals when pressing buttons

--- app/src/main/java/com/example/arduinotankapp/MainActivity.java
+++ app/src/main/java/com/example/arduinotankapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,32 +12,52 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button left = findViewById(R.id.leftButton);
-    Button right = findViewById(R.id.rightButton);
-    Button forward = findViewById(R.id.fwdButton);
-    Button reverse = findViewById(R.id.revButton);
-    TextView txtDisplay = findViewById(R.id.mainTextDisplay);
-
-    EditText tankIp = findViewById(R.id.ipInput);
+    Button left ;
+    Button right;
+    Button forward;
+    Button reverse;
+    TextView txtDisplay;
+    TextView ipDisplay;
+    EditText tankIp;
+    WebView webDisplay;
+    String tankAddress;
 
     public void onSubmit(View v) {
-        CharSequence ip = tankIp.getText();
+        String ip = tankIp.getText().toString();
+        ipDisplay.setText(ip);
+        tankAddress = "HTTP://" + ip;
+        txtDisplay.setText("website address:" + tankAddress);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+         left = findViewById(R.id.leftButton);
+         right = findViewById(R.id.rightButton);
+         forward = findViewById(R.id.fwdButton);
+         reverse = findViewById(R.id.revButton);
+         txtDisplay = findViewById(R.id.mainTextDisplay);
+         ipDisplay = findViewById(R.id.showIp);
+         tankIp = findViewById(R.id.ipInput);
+         webDisplay = findViewById(R.id.tankWebServer);
+
+        getSupportActionBar().hide();
 
         forward.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    //send signal to move forward
+                    //Send signal to move forward
+                    webDisplay.loadUrl(tankAddress + "/F");
                     txtDisplay.setText("Going Forward");
                 }
                 if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    //send stop signal
+                    //Send stop signal
+                    webDisplay.loadUrl(tankAddress + "/S");
                     txtDisplay.setText("");
                 }
                 return false;
@@ -46,11 +67,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    //send signal to back up
+                    //Send signal to back up
+                    webDisplay.loadUrl(tankAddress + "/B");
                     txtDisplay.setText("Backing Up");
                 }
                 if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    //send stop signal
+                    //Send stop signal
+                    webDisplay.loadUrl(tankAddress + "/S");
                     txtDisplay.setText("");
                 }
                 return false;
@@ -60,11 +83,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    //send signal to move left
+                    //Send signal to turn left
+                    webDisplay.loadUrl(tankAddress + "/L");
                     txtDisplay.setText("Turning Left");
                 }
                 if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    //send stop signal
+                    //Send stop signal
+                    webDisplay.loadUrl(tankAddress + "/S");
                     txtDisplay.setText("");
                 }
                 return false;
@@ -74,11 +99,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    //send signal to move right
+                    //Send signal to turn right
+                    webDisplay.loadUrl(tankAddress + "/R");
                     txtDisplay.setText("Turning Right");
                 }
                 if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    //send stop signal
+                    //Send stop signal
+                    webDisplay.loadUrl(tankAddress + "/S");
                     txtDisplay.setText("");
                 }
                 return false;
